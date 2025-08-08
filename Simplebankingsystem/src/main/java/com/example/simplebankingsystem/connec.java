6c9f1704e21952b6d690eb721c6aebcbafd489e6
Aditya-kumar2005 <nanuadityakumar@gmail.com>
2025-08-08 06:55:49 +0530

    Simple banking system is working

---
+++ Simplebankingsystem/src/main/java/com/example/simplebankingsystem/connec.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.simplebankingsystem;
+
+
+import java.sql.*;
+
+/**
+ *
+ * @author nanua
+ */
+public class connec {
+    Connection c;
+    Statement s;
+    public connec(){
+        try{
+           String url="jdbc:mysql:///simplebankingsystem";
+           c=DriverManager.getConnection(url,"root","Adity@25062005");
+           s=c.createStatement();
+        }
+        catch(Exception e){
+           System.out.print(e);
+        }
+    }
+}
